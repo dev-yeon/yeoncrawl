21231f8a4124bcd30c5ad535229ff481be012918
yeon <yeon@yeonui-MacBookAir.local>
2023-03-29 17:30:27 +0900

    logic

--- yeoncrawl/insta.py
+++ yeoncrawl/insta.py
@@ -1,10 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-
 import yeoncrawl.insta_utils
-
 import yeoncrawl.metadata as metadata
 from bs4 import BeautifulSoup
 import time
@@ -19,15 +15,11 @@ def insta_soup(request):
     #chrome_options = Options()
     #chrome_options.add_argument(
     #    'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36')
-
    # driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-
     driver = yeoncrawl.insta_utils.driver(request)
-
     # 인스타그램 접속
     driver.get(metadata.LOGIN_URL)
     time.sleep(2)
-
     # 로그인
     """
     username = metadata.INSTAGRAM_ID
@@ -41,36 +33,50 @@ def insta_soup(request):
     time.sleep(3)
     """
     login = yeoncrawl.insta_utils.instagram_login(request)
-
     # 해시태그 검색
-
     driver.get(f"{metadata.CONTENT_URL}{metadata.HASH_TAG}/")
     time.sleep(60)
-
     # 게시물 가져오기
     item_soup = BeautifulSoup(driver.page_source, 'html.parser')
     #  articles = soup.select('article')
-
     items = item_soup.select('div._aabd._aa8k._al3l')
     # div._aabd._aa8k._al3l 라는 형식으로 인스타의 아이템들은 구성되어있다 .
-
-    results = []
-
     return item_soup
 
 # 인기게시물 9개 읽기 (하드코딩)
 def hayatteru_9(request):
     driver = yeoncrawl.insta_utils.driver(request)
-    for j in range(1, 4):
-        for k in range(1, 4):
-            i_path = f'//*[@id="mount_0_0_fC"]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/article/div[1]/div/div/div[{j}]/div[{k}]'
+    for i in range(1, 4):
+        for j in range(1, 4):
+            for k in range(1, 4):
+                i_path = f'//*[@id="mount_0_0_fC"]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/article/div[{i}]/div/div/div[{j}]/div[{k}]'
+                i_button = driver.find_element(By.XPATH, i_path)
+                i_button.click()
+                time.sleep(3)
+                temp_post = Post()  # models 의 Post 호출
+                temp_post.save() #한번 세이브해서 일단 Post db에 새로운 데이터 생성
+                #------------------------
+                temp_postimg = PostImg()  # 새로운 이미지 저장할 그릇 생성
+                temp_postimg.img_url = ['srcset', 'x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3']
+                temp_postimg.save()
+                temp_post.img_list.add(temp_postimg)
+                temp_post.save()
+                count = 0
+                count = count+i  #  총 저장 갯수를 저장하는 변수 count가 있다. 이 count는 9개를 읽으면 [i] [j] [k]에서
+                if (k==4):      # count  가 4 가 되면, k를 1로 초기화 한다.
+                    k ==1
+                    continue    # for문 밖으로 빠져나온다
 
-            i_button = driver.find_element(By.XPATH, i_path)
-            i_button.click()
-            time.sleep(3)
-            temp_post = Post()
-            temp_post.post_desc = haya_1pic['alt']
-            temp_post.save()
+
+
+
+
+
+
+            # <img> 태그의 alt 속성은 이미지를 보여줄 수 없을 때 해당 이미지를 대체할 텍스트를 명시합니다.
+
+
+            """
             haya_item_html = driver.page_source
             haya_item_soup = BeautifulSoup(haya_item_html, "html.parser")
 # //*[@id="mount_0_0_Zl"]/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[1]/div/div[1]/div[2]/div/div/div/ul/li[2]/div/div/div/div/div[1]/div[1]/img
@@ -86,24 +92,7 @@ def hayatteru_9(request):
 
             haya_next_curser = haya_item_soup.find('div', class_= " _9zm2")
             # //*[@id="mount_0_0_Zl"]/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[1]/div/div[1]/div[2]/div/button/div
-            #  (인기순 9) 사진_저장
-            # (인기순 9)제일 처음에 뜨는 사진 css
-
-            # (인기순 9): 처음 이후 가려진 사진 css, 사진은 최대 10 장 저장이 가능하댄다 ...
-            # (인기순 9): 화살표 눌러서 옆으로 이동
-            #  (인기순 9)옆으로 눌러서 사진 저장 ~till 화살표 가 없을 때까지
-
-
-            # (인기순 9) : 업로드 한 사람 아이디
-            # (인기순 9) :  업로드 한 사람의 게시글 을 올린 장소 (없을 수도, 있을 수도)
-
-            #  (인기순 9) : 업로드 한 사람이 사진에 덧붙여 올린 글
-            # (인기순9) : 그리고 그 글에서 읽어올 수 있는 #해시 태그
-
-            # (인기순 9) : 그 글이 좋아요 받은 갯수
-            # (인기순 9) : 그 글을 올린 날짜
-
-            # 첫 번째 게시물 클릭
+            """
 
             """
             insta_utils.driver().find_element_by_css_selector("div.v1Nh3.kIKUG._bz0w").click()
